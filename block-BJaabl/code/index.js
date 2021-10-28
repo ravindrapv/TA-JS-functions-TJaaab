9e6ab719056635d818b4146898102e729a45cb3f
ravindrapv <ravindra.ravi4849@gmail.com>
2021-10-28 12:50:33 +0530

    functions

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -1,11 +1,10 @@
 /*
 1. Create a function named `sayHello` that that accepts a parameter `name` and alert `Hello [name]!`.
 */
-function sayHello(name){
+function sayHello(name) {
   return `hello ${name};`
 }
-let name =sayHello("ravindra");
-alert(`${name}`);
+
 /*
 2. Create a function named `getFullName` that accepts two parameter `firstName` and `lastName` and returns
 fullName.
@@ -14,12 +13,11 @@ Example:
   getFullName("John", "Snow"); // "John Snow"
   getFullName("Nelson", "Mandela"); // "Nelson Mandela"
 */
-function getFullName(firstName,lastName) {
-   
-  return firstName + lastName;
+function getFullName(firstName, lastName) {
+
+  return `${firstName} ${lastName}`;
 }
-let fullName = getFullName("venkata","ravindra");
-alert(`${fullName}`);
+
 /*
 3. Create a function named `addTwoNumbers` that accepts two numbers i.e `firstNum` and `secondNum` and returns
 the sum of both numbers. If user passes any data other then numbers alert a message saying `Enter Valid Input`
@@ -29,14 +27,17 @@ addTwoNumbers(10, 22); // 32
 addTwoNumbers(20, 32); // 32
 addTwoNumbers(10, "100"); // Alert Enter Valid Input
 */
-function addTwoNumbers() {
-  let firstNum = +prompt("enter first number");
-  let secondNum = +prompt("enter second number");
-  let sum = firstNum + secondNum;
-  return sum;
+function addTwoNumbers(firstNum, secondNum) {
+  if (typeof firstNum !== "number" || typeof secondNum !== "number");
+  {
+    alert("enter a valid input");
+  } 
+  {
+    return firstNum + secondNum;
+  }
 }
-sum = addTwoNumbers();
-alert(`${sum}`);
+
+
 /*
 4. Create a function named `calc` which accepts three parameter `numA`, `numB` and `operation`. Operation
 can only be one of these `add, sub, mul, div`. Based on the operator return the value. If the `numA` or `numB`
@@ -48,30 +49,27 @@ calc(10, 20, 'add'); // 30
 calc(20, 10, 'sub'); // 10
 calc(20, 10, 'mul'); // 200
 */
-function calc() {
-  let numA = +prompt("enter a number");
-  let numB = +prompt("enter another number");
-  let operation = prompt("enter any one operation(add, sub, mul, div)");
-  switch (operation) {
-    case add:
-      return(`${numA+numB}`);
-      break;
+function calc(firstNum, secondNum, operation) {
+  if (typeof firstNum !== "number" ||
+   typeof secondNum !== "number");
+  {
+    alert("enter a valid input");
+  }  
+    switch (operation) {
+      case add:
+        return firstNum + secondNum;
+        break;
       case sub:
-      alert(`${numA-numB}`);
-      break;
-      case mul:
-      alert(`${numA*numB}`);
-      break;
-      case div:
-      alert(`${numA%numB}`);
-      break;
-    default:
-      alert("enter a valid input");
-      break;
+        return firstNum - secondNum;
+        break;
+      case add:
+        return firstNum * secondNum;
+        break;
+      default:
+        alert(`enter a valid input`);
+        break;
   }
-  return add,sub,mul,div;
 }
-let add = calc();
 /*
 5. Create a function named `isLeapYear` that accepts a number data type and return `true` or `false` based
 on if the year id leap year or not.
@@ -79,8 +77,19 @@ on if the year id leap year or not.
 isLeapYear(2000); // true
 isLeapYear(2001); // false
 */
-
+function isLeapyear(year) {
+  if(year % 400 === 0)return true;
+  if(year % 100 === 0)return false;
+  return year %4 === 0;
+}
 
 /*
 6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.
 */
+function getFactorial(num) {
+  let final = 1;
+  for(let i=1; i>num; i--) {
+    final *=i;
+  }
+  return final;
+}
